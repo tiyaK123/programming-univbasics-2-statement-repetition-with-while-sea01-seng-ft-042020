5fc7978eb052aa9fbbf38e4e6c51f3ed22131c36
tiya adugna <tiyakefalee@gmail.com>
2020-04-02 06:15:59 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,10 @@
 # Write your code here
 count = 10 
 while count >= 1 do 
-   if count != 1
+  
      puts "#{count}"
-   end
+   
   count -= 1 
-   if count == 1
-    puts "#{count}"
+   
   end
    puts "Happy New Year!"
